56a1bcd175b83c6c79c48d8ba6174e976ed362f1
Arnav S Rao <arnavsrao09@gmail.com>
2025-03-23 17:04:30 +0530

    Fixed delete and edit bug

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -2,10 +2,12 @@ export function Tabs(props){
   const { todos, selectedTab, setSelectedTab } = props
   const tabs = ['All', 'Open', 'Completed']
   return(
-    <nav>
+    <nav className="tab-container">
       {tabs.map((tab, tabIndex) => {
-        const numOfTasks = tab === 'All' ? todos.length :
-          tab === 'Completed' ? todos.filter((todo) => todo.completed).length: 
+        const numOfTasks = tab === 'All' ? 
+        todos.length :
+          tab === 'Completed' ? 
+          todos.filter((todo) => todo.completed).length: 
           todos.filter((todo) => !todo.completed).length
         return (
           <button 
@@ -14,12 +16,13 @@ export function Tabs(props){
           } 
           key={tabIndex} 
           className={"tab-button "
-          + (tab === selectedTab ? 'tab-selected' : '')
+          + (tab === selectedTab ? 'tab-selected' : ' ')
           }>
-            {tab}({numOfTasks})
+            <h4>{tab}<span>({numOfTasks})</span></h4>
           </button>
         )
       })}
+      <hr />
     </nav>
   )
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,20 +1,19 @@
 import { TodoCard } from "./TodoCard";
 
 export function TodoList(props){
-  const { todos, selectedTab } = props;
+  const { todos, selectedTab } = props
   const filteredTodoList = selectedTab === 'All'?
     todos :
     selectedTab === 'Completed'? 
     todos.filter(todo => todo.completed) : 
     todos.filter(todo => !todo.completed)
-  console.log(filteredTodoList)
   return(
     <>
       {filteredTodoList.map((todo,todoIndex) => {
         return(
           <TodoCard 
           key={todoIndex} 
-          todoIndex={todos.findIndex(val => val.input === todo.input)}
+          todoIndex={todos.findIndex(val => val.title === todo.title)}
           todo={todo} 
           {...props}/>
         )
